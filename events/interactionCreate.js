84c9d608991196cd8ad6996286e855fbc73d05e8
Auroiah Morgan <amwebdev@outlook.com>
2023-04-24 08:31:16 -0400

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,22 +2,19 @@ module.exports = {
 	name: 'interactionCreate',
 	async execute(interaction) {
 		if (interaction.isModalSubmit()) {
-			// const favoriteColor = interaction.fields.getTextInputValue('favoriteColorInput');
-			// console.log(favoriteColor);
 			if (interaction.customId === 'myModal') {
 				await interaction.reply({ content: 'Your submission was received successfully!' });
-			} else if (interaction.customId === 'createQuackathon'){
-				console.log("modal submitted...")
+			} else if (interaction.customId === 'createQuackathon') {
+				console.log('modal submitted...');
 				const res1 = await interaction.fields.getTextInputValue('titleInput');
-				console.log("res1: ", res1)
+				console.log('res1: ', res1);
 				const res2 = await interaction.fields.getTextInputValue('descriptionInput');
 				const res3 = await interaction.fields.getTextInputValue('requirementsInput');
 				console.log(res1, res2, res3);
-				
-				await interaction.reply({content: "Quackathon Created!!"});
-				
+
+				await interaction.reply({ content: 'Quackathon Created!!' });
 			} else {
-				await interaction.reply({content: "Quackathon Created!!"});
+				await interaction.reply({ content: 'Quackathon Created!!' });
 			}
 		}
 		if (!interaction.isCommand()) return;
