85a78a4f959fe8d3bd8958cb206960e6559bedae
guitarkeegan <keegananglim@gmail.com>
2023-04-26 11:45:20 -0700

    issues with submit project test

--- functions-without-context.ts
+++ functions-without-context.ts
@@ -1,3 +1,4 @@
+import { UsersOnProjects } from '@prisma/client';
 import prisma from './client'
 
 interface CreateQuackathon {
@@ -44,4 +45,68 @@ export async function createProjectTeam(projectTeam: CreateProjectTeam) {
 		},
 		
 	});
+}
+
+export async function createUser(userId: string) {
+	return await prisma.user.create({
+		data: {
+			id: userId,
+		},
+	});
+}
+
+export async function submitProject(projectTeam: string, userId: string) {
+
+	// check if project exists
+	const project = await prisma.projectTeam.findUnique({
+		where: {
+			projectName: projectTeam,
+		},
+		select: {
+			id: true,
+			isSubmitted: true,
+			members: true,
+			challengeId: true,
+		},
+	});
+
+	if (!project) {
+		throw new Error('Project does not exist');
+	}
+	// check if user is a member of the project
+	if (!project.members.includes(userId as unknown as UsersOnProjects)) {
+		throw new Error('User is not a member of this project');
+	}
+	// check if project is submitted
+	if (project.isSubmitted) {
+		throw new Error('Project is already submitted');
+	}
+	// check if project is submitted after due date
+	const challenge = await prisma.challenge.findUnique({
+		where: {
+			id: project.challengeId,
+		},
+		select: {
+			dueAt: true,
+		}
+	});
+	
+	if (challenge && challenge.dueAt < new Date()) {
+		throw new Error('Project is submitted after due date');
+	}
+	try {
+		return await prisma.projectTeam.update({
+			where: {
+				id: project.id,
+			},
+			data: {
+				isSubmitted: true,
+			},
+		});
+	} catch (error){
+		console.error("error on update", error)
+	}
+
+
+	
 }
