f8b7cc75467a6ddb0daa161f5d1072ac7d82ff36
guitarkeegan <keegananglim@gmail.com>
2023-04-27 11:33:04 -0700

    refactor schema

---
+++ prisma/migrations/20230427182951_init/migration.sql
@@ -0,0 +1,60 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "challengeId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Team_challengeId_fkey" FOREIGN KEY ("challengeId") REFERENCES "Challenge" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "UsersOnTeams" (
+    "userId" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("userId", "teamId"),
+    CONSTRAINT "UsersOnTeams_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "UsersOnTeams_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Submission" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "projectName" TEXT NOT NULL,
+    "teamId" TEXT NOT NULL,
+    "repoUrl" TEXT NOT NULL,
+    "deployedUrl" TEXT,
+    "winner" BOOLEAN NOT NULL DEFAULT false,
+    "isSubmitted" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Submission_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Challenge" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "dueAt" DATETIME NOT NULL,
+    "minTeam" INTEGER DEFAULT 2,
+    "maxTeam" INTEGER DEFAULT 4,
+    "description" TEXT NOT NULL,
+    "requirements" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Submission_projectName_key" ON "Submission"("projectName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Submission_teamId_key" ON "Submission"("teamId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Challenge_title_key" ON "Challenge"("title");
