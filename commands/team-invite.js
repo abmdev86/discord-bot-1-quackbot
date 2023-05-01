d0dfe2f200a6c3e6fc5ccbf5cbdc9df490dabbe5
Auroiah Morgan <amwebdev@outlook.com>
2023-04-30 20:11:50 -0400

    Add team-iinvite

--- commands/team-invite.js
+++ commands/team-invite.js
@@ -1,14 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-
+const { getUserById } = require('../lib/dbHandlers');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('team-invite')
 		.setDescription('Invite a member to your team')
-		.addStringOption(option => option.setName('input').setDescription('The name of user').setRequired(true)),
+		.addStringOption(option => option.setName('user').setDescription('The name of user').setRequired(true)),
 
 	async execute(interaction) {
 		try {
-			await interaction.deferReply(); // defered and handled on event
+			console.log(interaction.user);
+			const userId = interaction.user.id;
+
+			const user = await getUserById(userId, true);
+
+			if (user) {
+				//todo
+				const otherMember = interaction.options._hoistedOptions[0].value;
+				console.log('OTHER MEMEBER ', otherMember);
+				const newUser = await getUserById(otherMember.id, true);
+
+				await interaction.reply(`Invited ${newUser.id}`);
+			} else {
+				interaction.reply('You must be a registered user to invite. try /register command first');
+			}
 		} catch (error) {
 			console.error('AN ERROR', error);
 			await interaction.reply({ content: 'An Error Occurred!', ephemeral: true });
